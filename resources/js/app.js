4bcf145b34ce431143ab8831219de82afd7e1804
Jordancbo Eftimov <ejordancho@gmail.com>
2022-02-02 21:03:09 +0100

    Init inertia js project

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import {createInertiaApp} from '@inertiajs/inertia-vue'
+import {InertiaProgress} from '@inertiajs/progress'
+
+Vue.prototype.$route = route
+InertiaProgress.init()
+createInertiaApp({
+    resolve: name => require(`./Pages/${name}`),
+    setup({el, App, props, plugin}) {
+        Vue.use(plugin)
+
+        new Vue({
+            render: h => h(App, props),
+        }).$mount(el)
+    },
+})
