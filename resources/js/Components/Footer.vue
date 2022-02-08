097986c1d3054a202a4a05e3f2c8864e6a155f42
Jordancbo Eftimov <ejordancho@gmail.com>
2022-02-08 22:12:01 +0100

    Footer component done

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer class="py-3 text-center text-white fw-bold bg-gradient fs-6">
+        Copyright &copy; iVid. All Rights Reserved.
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+.bg-gradient {
+    background: rgb(11, 88, 134) !important;
+    background: linear-gradient(90deg, rgba(11, 88, 134, 1) 0%, rgba(56, 182, 255, 1) 50%) !important;
+}
+</style>
