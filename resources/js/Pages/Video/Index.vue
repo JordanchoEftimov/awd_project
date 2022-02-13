93cbc096751e342e33b7783679046c487b1837d2
Jordancbo Eftimov <ejordancho@gmail.com>
2022-02-13 16:45:04 +0100

    Create video on videos index button works

--- resources/js/Pages/Video/Index.vue
+++ resources/js/Pages/Video/Index.vue
@@ -3,7 +3,9 @@
         <div
             class="d-flex flex-column flex-sm-row align-items-center justify-content-center justify-content-sm-between mb-4">
             <div class="display-6 mb-3 mb-sm-0">My Videos</div>
-            <Link class="btn btn-primary border-radius-20px fw-bold text-white px-4">Create Video</Link>
+            <Link :href="$route('videos.create')" class="btn btn-primary border-radius-20px fw-bold text-white px-4">
+                Create Video
+            </Link>
         </div>
         <div class="row">
             <div v-for="i in 20" class="col col-12 col-md-6 col-xl-4 mb-4">
